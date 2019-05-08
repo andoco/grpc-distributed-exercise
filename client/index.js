6bd03cfcd5768e5b3d0103ed34f8bcb9fa11a6d5
Andrew O'Connor <andrew.oconnor@gmail.com>
2019-05-08 17:14:14 +0100

    Receive max values and sum

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 var parseArgs = require('minimist')
-var args = parseArgs(process.argv.slice(2), { default: { port: 8888 } });
+var args = parseArgs(process.argv.slice(2), { default: { port: 8888, maxReceive: 0xffff } });
 
 var PROTO_PATH = __dirname + '/../protos/numbers.proto';
 var grpc = require('grpc');
@@ -20,8 +20,17 @@ var stub = new numbers.Generator(`localhost:${args['port']}`, grpc.credentials.c
 
 var call = stub.begin();
 
+var totalReceived = 0
+var sum = 0
+
 call.on('data', number => {
-  console.log('received data', number.value)
+  totalReceived++
+  sum += number.value
+  console.log('value:', number.value, 'received:', totalReceived, 'sum:', sum)
+  if (totalReceived >= args.maxReceive) {
+    console.log('sum:', sum)
+    process.exit()
+  }
 })
 call.on('end', () => {
   console.log('server has finished sending')
