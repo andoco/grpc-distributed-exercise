ec7da2740818cd6da176f2c2963fe375454dc741
Andrew O'Connor <andrew.oconnor@gmail.com>
2019-05-08 21:13:38 +0100

    Retry connect on stream error
    Exponential retry of connect on any error.

--- client/index.js
+++ client/index.js
@@ -1,4 +1,6 @@
 var parseArgs = require('minimist')
+var backoff = require('backoff');
+
 var args = parseArgs(process.argv.slice(2), { default: { port: 8888, maxReceive: 0xffff } });
 
 var PROTO_PATH = __dirname + '/../protos/numbers.proto';
@@ -18,12 +20,46 @@ var numbers = grpc.loadPackageDefinition(packageDefinition).numbers;
 
 var stub = new numbers.Generator(`localhost:${args['port']}`, grpc.credentials.createInsecure())
 
-var call = stub.begin();
+var connectBackoff = backoff.exponential({ randomisationFactor: 0.1 })
+connectBackoff.failAfter(10)
+
+connectBackoff.on('backoff', (number, delay) => {
+  console.log('backoff:start', 'number:', number, 'delay:', delay)
+})
+connectBackoff.on('ready', (number, delay) => {
+  console.log('backoff:ready', 'number:', number, 'delay:', delay)
+  connect()
+})
+connectBackoff.on('fail', () => {
+  console.log('backoff:fail')
+})
 
 var totalReceived = 0
 var sum = 0
 
-call.on('data', number => {
+connect()
+
+function connect() {
+  var call = totalReceived == 0 ? stub.Begin() : stub.Resume({ seed: sum })
+
+  call.on('data', number => {
+    connectBackoff.reset()
+    handleNumber(number)
+  })
+  call.on('end', () => {
+    console.log('server has finished sending')
+  })
+  call.on('error', err => {
+    console.error('error', err)
+    call.destroy()
+    connectBackoff.backoff(err)
+  })
+  call.on('status', status => {
+    console.log('status', status)
+  })
+}
+
+function handleNumber(number) {
   totalReceived++
   sum += number.value
   console.log('value:', number.value, 'received:', totalReceived, 'sum:', sum)
@@ -31,13 +67,4 @@ call.on('data', number => {
     console.log('sum:', sum)
     process.exit()
   }
-})
-call.on('end', () => {
-  console.log('server has finished sending')
-})
-call.on('error', e => {
-  console.error('error', e)
-})
-call.on('status', status => {
-  console.log('status', status)
-})
+}
