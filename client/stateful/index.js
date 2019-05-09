e6a8b556a6b56d7875cd9ba99e734fb6d87c850d
Andrew O'Connor <andrew.oconnor@gmail.com>
2019-05-09 09:24:25 +0100

    Initial stateful client code

--- client/stateful/index.js
+++ client/stateful/index.js
@@ -1,5 +1,58 @@
-function run() {
+var stub
+
+function setupGrpc(args) {
+  var PROTO_PATH = __dirname + '/../../protos/randstream.proto';
+  var grpc = require('grpc');
+  var protoLoader = require('@grpc/proto-loader');
+  // Suggested options for similarity to existing grpc.load behavior
+  var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+      keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true
+    });
+  var randstream = grpc.loadPackageDefinition(packageDefinition).randstream;
+
+  stub = new randstream.Generator(`localhost:${args['port']}`, grpc.credentials.createInsecure())
 }
+
+function connect() {
+  var call = stub.Begin({ clientId: "test", maxNumbers: 10 })
+
+  call.on('data', number => {
+    handleNumber(number)
+  })
+  call.on('end', () => {
+    console.log('server has finished sending')
+  })
+  call.on('error', err => {
+    console.error('error', err)
+    call.destroy()
+  })
+  call.on('status', status => {
+    console.log('status', status)
+  })
+}
+
+function handleNumber(number) {
+  totalReceived++
+  console.log('value:', number.value, 'received:', totalReceived)
+
+  if (number.checksum) {
+    console.log('received checksum:', number.checksum)
+  }
+}
+
+var totalReceived = 0
+
+function run(args) {
+  setupGrpc(args)
+  connect()
+}
+
 
 module.exports = {
   run
