09e1fa2eb05151bde9fb5a3e3016cf7c8fb3b15c
Andrew O'Connor <andrew.oconnor@gmail.com>
2019-05-09 08:48:30 +0100

    Initial stateful Begin rcp func

--- server/stateful/stateful.go
+++ server/stateful/stateful.go
@@ -1,5 +1,33 @@
 package stateful
 
+import (
+	"fmt"
+	"net"
+
+	"github.com/andoco/ably-distributed-exercise/server/stateful/randstream"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+)
+
+type service struct{}
+
+func (s *service) Begin(req *randstream.BeginRequest, stream randstream.Generator_BeginServer) error {
+	stream.Send(&randstream.Number{Value: 1})
+	return nil
+}
+
 func Serve(port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return errors.Wrap(err, "failed to listen")
+	}
+
+	grpcServer := grpc.NewServer()
+	randstream.RegisterGeneratorServer(grpcServer, &service{})
+
+	if err := grpcServer.Serve(lis); err != nil {
+		return errors.Wrap(err, "serve")
+	}
+
 	return nil
 }
